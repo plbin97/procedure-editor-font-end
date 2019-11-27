27dc23b72568b9ef2fec1c6565472dc77b681c30
plbin97 <i@teenet.me>
2019-11-26 19:27:04 -0500

    new

---
+++ build/precache-manifest.b0e8199ac20ad9e6a30bf2633d62667e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dad90779596bd886849527ec14f65d6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "31a6ac76d48422c7e4ef",
+    "url": "./static/css/main.cba46eb7.chunk.css"
+  },
+  {
+    "revision": "5519ffee1f3447f33cb1",
+    "url": "./static/js/2.432b4a14.chunk.js"
+  },
+  {
+    "revision": "31a6ac76d48422c7e4ef",
+    "url": "./static/js/main.3d7a0993.chunk.js"
+  },
+  {
+    "revision": "4e11739fd52abab7c070",
+    "url": "./static/js/runtime-main.993cf12f.js"
+  }
+]);
